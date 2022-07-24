cf40507955009b943ab79201cf0b2b774943717d
Tiago Tavares <tiagotvrs@gmail.com>
2022-07-24 11:43:27 -0300

    Melhorias

--- run2.sh
+++ run2.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-#Root or Sudoer verifying.
-if [[ $(id -u) != 0 ]]; then
-    echo -e "\n[!] Install.sh need to run as root or sudoer"
-    exit 0
-else
-    callFunctions
-fi
-
-
-callFunctions (){
+callRequirements(){
     setupEnvironment
     setupOSRequirements
     setupGolang
 }
 
 setupEnvironment() {
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
     LOCALPATH=$(pwd)
     TOOLSPATH="/opt/tools"
     WLPATH="/opt/wordlists"
@@ -42,23 +34,48 @@ setupEnvironment() {
 }
 
 setupOSRequirements (){
-    echo -e "${RED}[+] Instalando pre requisitos${NC}"
-#Installing packages
-apt-get update 
-apt dist-upgrade -y 
-apt-get install default-jre zsh p7zip curl sqlmap john net-tools \
-locate libpcap-dev htop vim gzip ruby-dev zip git python3-pip python-is-python3 \
-jq tmux snap grepcidr nmap masscan brutespray prips azure-cli fonts-powerline -y
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    #Installing packages
+    apt-get update 
+    apt dist-upgrade -y 
+    apt-get install \
+    brutespray \
+    curl \
+    default-jre \
+    fonts-powerline \
+    git \
+    grepcidr \
+    gzip \
+    htop \
+    john \
+    jq \
+    libpcap-dev \
+    locate \
+    masscan \
+    net-tools \
+    nmap \
+    p7zip \
+    prips \
+    python3-pip \
+    python-is-python3 \
+    ruby-dev \
+    snap \
+    sqlmap \
+    tmux \
+    vim \
+    zip \
+    zsh \
+    -y
 }
 
 setupGolang () {
     #Installing newer GO
-    echo -e "${RED}[+] Intalando Golang${NC}"
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
     apt purge golang -y
     apt autoremove golang -y
     cd /tmp
     wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     #echo "export PATH=$PATH:/usr/local/go/bin:/home/$SUDO_USER/go/bin" >> ~/.bashrc
     #export GOROOT=/usr/local/go
@@ -66,3 +83,10 @@ setupGolang () {
 }
 
 
+#Root or Sudoer verifying.
+if [[ $(id -u) != 0 ]]; then
+    echo -e "\n[!] Install.sh need to run as root or sudoer"
+    exit 0
+else
+    callRequirements
+fi
