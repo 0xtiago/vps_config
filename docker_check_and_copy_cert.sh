72da1caf42e23763cc1452b3eed59b4c34e0cabc
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-27 16:38:44 -0300

    $(PWD)/burp_cert.crt"

--- docker_check_and_copy_cert.sh
+++ docker_check_and_copy_cert.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-CERT_PATH="burp_cert.crt"
+CERT_PATH="$(PWD)/burp_cert.crt"
 
 if [ -f "$CERT_PATH" ]; then
     echo "Burp certificate "$CERT_PATH" found. Importing..."
+    cp $CERT_PATH /usr/local/share/ca-certificates/burp_cert.crt
     update-ca-certificates
 else
     echo "Burp Certificate "$CERT_PATH" not found. Skipping..."
