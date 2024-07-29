4c700172ce936850830d274ef378c85580fe57c0
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-29 08:29:34 -0300

    Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,57 +1,44 @@
 FROM ubuntu:latest
 
-ENV HOSTNAME=Baleiao
-
 RUN apt update && apt install -y \
     zsh \
     git \
     curl \
     wget \
-    golang \
-    python3 \
-    python3-pip
-
-
-WORKDIR /workdir
-
+    vim \
+    tmux
 
 # Instalando o ohmyzsh e configurando o zsh como bash padrão
 # https://ohmyz.sh/#install
-RUN sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" && \
+RUN sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
     chsh -s /bin/zsh
 
-# Spaceship
-RUN git clone https://github.com/denysdovhan/spaceship-prompt.git "/root/.oh-my-zsh/custom/themes/spaceship-prompt" && \
-    ln -s "/root/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/root/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
-
-# Configuração para o zsh history e Spaceship
-RUN echo 'ZSH_THEME="spaceship"' >> /root/.zshrc && \
-    echo 'HISTFILE=/workdir/.zsh_history_docker' >> /root/.zshrc && \
+# Configuração para o zsh history
+RUN echo 'HISTFILE=/workdir/.zsh_history_docker' >> /root/.zshrc && \
     echo 'HISTSIZE=1000' >> /root/.zshrc && \
     echo 'SAVEHIST=1000' >> /root/.zshrc && \
     echo 'setopt inc_append_history' >> /root/.zshrc && \
-    echo 'setopt share_history' >> /root/.zshrc && \
-    echo 'SPACESHIP_PROMPT_ORDER=("user" "dir" "git" "host" "exec_time" "line_sep" "jobs" "exit_code" "char")' >> /root/.zshrc && \
-    echo 'SPACESHIP_DIR_TRUNC=0' >> /root/.zshrc && \
-    echo 'SPACESHIP_DIR_PREFIX="🐳 "' >> /root/.zshrc && \
-    echo 'SPACESHIP_CHAR_SYMBOL="➜ "' >> /root/.zshrc && \
-    echo 'SPACESHIP_CHAR_SUFFIX=""' >> /root/.zshrc
+    echo 'setopt share_history' >> /root/.zshrc
 
-# Cria .zsh_history
-# Vc precisa criar o .zsh_history localmente para o mapeamento!
-# $ touch .zsh_history
-# RUN touch /root/.zsh_history
-
-# Executa script básico de instalação de configuração do container
-RUN wget -O - "https://raw.githubusercontent.com/0xtiago/vps_config/main/run_basic.sh" | bash
-
-
+RUN sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="refined"/' /root/.zshrc
 
 # Instalando o ohmytmux
-RUN git clone https://github.com/gpakosz/.tmux.git /root/.tmux && \
-    ln -s -f /root/.tmux/.tmux.conf /root/.tmux.conf && \
-    cp /root/.tmux/.tmux.conf.local /root/
+RUN git clone https://github.com/gpakosz/.tmux.git ~/.tmux && \
+    ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf && \
+    cp ~/.tmux/.tmux.conf.local ~/
 
+# Copia o script para o container
+COPY auxiliar_scripts/docker_script.sh /usr/local/bin/docker_script.sh
+
+# Define permissões de execução e executa o script
+# RUN chmod +x /usr/local/bin/docker_script.sh && \
+#     /usr/local/bin/docker_script.sh
+
+
+#Limpeza de cache para diminuir a imagem
+RUN rm -rf /var/lib/apt/lists/*
+
+WORKDIR /workdir
 
 # Para manter o container em execução após saídas
 CMD ["tail", "-f", "/dev/null"]
---
+++ auxiliar_scripts/docker_script.sh
@@ -0,0 +1,571 @@
+#!/bin/bash
+
+
+#INICIO DO GERA LOG DE TUDO
+# LOGFILE="/root/vps_config.log"
+# exec > >(tee -a $LOGFILE) 2>&1
+# #FIM DO GERA LOG DE TUDO ################################################
+
+# #VALIDA VERSAO DO LINUX
+# LINUXRELEASE=$(lsb_release -d | awk '{print $2,$3}')
+# if [[ $LINUXRELEASE =~ "Ubuntu 23." ]]; then
+#     PIPCOMMAND="pip install --break-system-packages"
+# else
+#     PIPCOMMAND="pip install"
+# fi
+
+#INICIO DE INSTALACAO DE PRE-REQUISITOS #################################
+
+setupEnvironment() {
+    #Cores
+    #========================================
+    #https://www.shellhacks.com/bash-colors/
+    RED='\e[31m'
+    GREEN='\e[32m'
+    CYAN='\e[36m'
+    PURPLE='\e[35m'
+    YELLOW='\e[33m'
+    NC='\e[0m' # No Color
+    #========================================
+
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    LOCALPATH=$(pwd)
+    TOOLSPATH="/opt/tools"
+    WLPATH="/opt/wordlists"
+    DIRBINPATH="/usr/local/bin"
+
+    #Diretorio de ferramentas
+    mkdir -p /opt/tools
+
+    #Diretorios de wordlists
+    mkdir -p /opt/wordlists
+
+}
+
+echo -e "${GREEN}This is an additional script to be executed during the container building.${NC}"
+
+setupOSRequirements (){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    #Installing packages
+    apt update 
+    apt install -y \
+    brutespray \
+    ca-certificates \
+    curl \
+    dos2unix \
+    default-jre \
+    fonts-powerline \
+    git \
+    gnupg2 \
+    golang \
+    gpg \
+    grepcidr \
+    gzip \
+    htop \
+    inetutils-ping \
+    john \
+    jq \
+    libpcap-dev \
+    locate \
+    masscan \
+    net-tools \
+    nmap \
+    p7zip \
+    prips \
+    python3-pip \
+    python-is-python3 \
+    ruby-dev \
+    snapd \
+    sqlmap \
+    tmux \
+    vim \
+    vim-nox \
+    zip \
+    zsh 
+}
+
+#INICIO DE INSTALAÇÃO DE FERRAMENTAS #################################
+
+Altdns(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND py-altdns==1.0.2 
+}
+
+Alterx(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/alterx/cmd/alterx@latest
+}
+
+Amass(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    snap install amass
+}
+
+Anew(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/anew@latest
+}
+
+Antiburl(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH} 
+    wget https://raw.githubusercontent.com/tomnomnom/hacks/master/anti-burl/main.go
+    go build main.go
+    rm -f main.go
+    mv main anti-burl && chmod +x anti-burl
+    ln -s ${TOOLSPATH}/anti-burl ${DIRBINPATH}/anti-burl
+}
+
+Arjun(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND arjun
+}
+
+Assetfinder(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/assetfinder@latest
+}
+
+Burl(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/burl@latest
+}
+
+ChaosClient(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
+}
+
+Collector(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    wget https://raw.githubusercontent.com/m4ll0k/Bug-Bounty-Toolz/master/collector.py
+    sed -i '#!/usr/bin/env python3' collector.py
+    chmod +x collector.py
+    ln -s ${TOOLSPATH}/collector.py ${DIRBINPATH}/collector.py
+}
+
+DalFox(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    snap install dalfox
+}
+
+Dirsearch(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND dirsearch
+}
+
+Dnsgen(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND dnsgen
+}
+
+Dnsx(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+}
+
+DNSValidaor(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/vortexau/dnsvalidator.git
+    cd dnsvalidator
+    python3 setup.py install
+}
+
+ffuf(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/ffuf/ffuf@latest
+}
+
+Findomains(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd /tmp
+    wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
+    unzip findomain-linux.zip
+    mv findomain ${DIRBINPATH}
+    chmod +x ${DIRBINPATH}/findomain
+}
+
+Gau(){
+    go install -v github.com/lc/gau@latest
+}
+
+Gauplus(){
+    go install -v  github.com/bp0lr/gauplus@latest
+}
+
+Gf(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/tomnomnom/gf.git
+    cd gf
+    go build main.go
+    mv main gf
+
+    mkdir ~/.gf
+    cp -r examples/* ~/.gf
+    cd /tmp
+    git clone https://github.com/1ndianl33t/Gf-Patterns && cd Gf-Patterns && cp *.json ~/.gf
+}
+
+GithubSearch(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/gwen001/github-search.git
+    cd github-search
+    $PIPCOMMAND -r requirements.txt
+    ln -s ${TOOLSPATH}/github-search/github-subdomains.py ${DIRBINPATH}/github-subdomains
+}
+
+
+GitDorker(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/obheda12/GitDorker.git
+    cd GitDorker
+    $PIPCOMMAND -r requirements.txt
+    chmod +x GitDorker.py
+    ln -s ${TOOLSPATH}/GitDorker/GitDorker.py ${DIRBINPATH}/gitdorker
+}
+
+GitDumper(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND git-dumper
+}
+
+GoogleChrome(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd /tmp
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    apt install ./google-chrome-stable_current_amd64.deb -y 
+}
+
+GoSpider(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/jaeles-project/gospider@latest
+}
+
+Gowitness(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/sensepost/gowitness@latest
+}
+
+Hakrawler(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/hakluke/hakrawler@latest
+}
+
+Hakrevdns(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/hakluke/hakrevdns@latest
+}
+
+Haktrails(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/hakluke/haktrails@latest
+}
+
+Httprobe(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/httprobe@latest
+}
+
+
+Httpx(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+}
+
+JSScanner(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/0x240x23elu/JSScanner.git
+    cd JSScanner
+    $PIPCOMMAND -r requirements.txt
+    chmod +x JSScanner.py
+    ln -s ${TOOLSPATH}/JSScanner/JSScanner.py ${DIRBINPATH}/jsscanner
+}
+
+JsubFinder(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install github.com/ThreatUnkown/jsubfinder@latest
+    wget https://raw.githubusercontent.com/ThreatUnkown/jsubfinder/master/.jsf_signatures.yaml && mv .jsf_signatures.yaml ~/.jsf_signatures.yaml
+}
+
+Kiterunner(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    wget https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz
+    tar xvzf kiterunner_1.0.2_linux_amd64.tar.gz
+    chmod +x kr 
+    ln -s ${TOOLSPATH}/kr ${DIRBINPATH}/kr
+}
+
+LinkFinder(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/GerbenJavado/LinkFinder
+    cd LinkFinder
+    $PIPCOMMAND -r requirements.txt
+    python3 setup.py install
+    ln -s ${TOOLSPATH}/LinkFinder/linkfinder.py ${DIRBINPATH}/linkfinder
+}
+
+Mapcidr(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+}
+
+Massdns(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH} 
+    git clone https://github.com/blechschmidt/massdns.git
+    cd massdns
+    make && make nolinux
+    cp -v ${TOOLSPATH}/massdns/bin/massdns ${DIRBINPATH}/massdns
+}
+
+MegaPy(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND mega.py
+}
+
+Metabigor(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/j3ssie/metabigor@latest
+}
+
+Metasploit(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd /tmp 
+    curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/metasploit-framework.gpg.key | sudo apt-key add -
+    sudo sh -c 'echo "deb http://downloads.metasploit.com/data/releases/metasploit-framework/apt lucid main" > /etc/apt/sources.list.d/metasploit-framework.list'
+    sudo apt update
+    sudo apt install -y metasploit-framework
+    msfupdate
+}
+
+Naabu(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+}
+
+Notify(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+   go install -v  github.com/projectdiscovery/notify/cmd/notify@latest
+}
+
+Nrich(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    local NRICH=https://gitlab.com/api/v4/projects/33695681/packages/generic/nrich/latest/nrich_latest_amd64.deb
+    cd /tmp && wget $NRICH
+    dpkg -i $(basename $NRICH)
+}
+
+Nuclei(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+    ~/go/bin/nuclei -update-templates
+}
+
+ParamSpider(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/devanshbatham/ParamSpider
+    cd ParamSpider
+    $PIPCOMMAND .
+    #ln -s ${TOOLSPATH}/ParamSpider/paramspider.py ${DIRBINPATH}/paramspider ## Já está sendo instalado
+}
+
+PureDNS(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/d3mondev/puredns/v2@latest
+}
+
+
+
+ShufleDNS(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+}
+
+sslscan(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+     apt install sslscan
+}
+
+
+
+Sub404(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH} 
+    git clone https://github.com/r3curs1v3-pr0xy/sub404.git
+    cd sub404
+    $PIPCOMMAND -r requirements.txt
+    chmod +x sub404.py
+    ln -s ${TOOLSPATH}/sub404/sub404.py ${DIRBINPATH}/sub404
+}
+
+Subfinder(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+}
+
+Subjs(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/lc/subjs@latest
+}
+
+Telegram-Send(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND telegram-send
+}
+
+TurboSearch(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND --upgrade turbosearch
+}
+
+Qsreplace(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v  github.com/tomnomnom/qsreplace@latest
+}
+
+Unfurl(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/unfurl@latest
+}
+
+Uro(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    $PIPCOMMAND uro
+}
+
+Waybackurls(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    go install -v github.com/tomnomnom/waybackurls@latest
+}
+
+wafw00f(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    cd ${TOOLSPATH}
+    git clone https://github.com/EnableSecurity/wafw00f
+    cd wafw00f
+    python3 setup.py install
+}
+
+WPScan(){
+    echo -e "${RED}[+]${FUNCNAME[0]}${NC}"
+    gem install wpscan
+}
+
+#FIM DE INSTALAÇÃO DE FERRAMENTAS #################################
+
+#INICIO DE CONFIGURACOES FINAIS #################################
+
+PosInstalacao(){
+    # if [ -d "/root/go/bin" ]; then
+    #     echo -e "${RED}[+]Moving files from /root/go/bin to ${DIRBINPATH}${NC}"
+    #     mv /root/go/bin/* ${DIRBINPATH}
+    # elif [ -d "/go/bin" ]; then
+    #     echo -e "${RED}[+]Moving files from /go/bin to ${DIRBINPATH}${NC}"
+    #     mv /go/bin/* ${DIRBINPATH}
+    # elif [ -d "/home/$SUDO_USER/go/bin" ]; then
+    #     echo -e "${RED}[+]Moving files from /home/$SUDO_USER/go/bin to ${DIRBINPATH}${NC}"
+    #     mv /home/$SUDO_USER/go/bin/* ${DIRBINPATH}
+    # else
+    #     echo -e "${RED}[+]Moving go binary not executed. Nothing to do.${NC}"
+    # fi
+
+    # ## Resolvendo problema da chave depreciada
+    # cd /tmp
+    # wget http://apt.metasploit.com/metasploit-framework.gpg.key
+    # gpg --no-default-keyring --keyring ./metasploit-framework_keyring.gpg --import metasploit-framework.gpg.key
+    # gpg --no-default-keyring --keyring ./metasploit-framework_keyring.gpg --export > ./metasploit-framework.gpg
+    # mv ./metasploit-framework.gpg /etc/apt/trusted.gpg.d/
+
+    echo -e "${GREEN}[+] DONE${NC}"
+
+}
+
+#FIM DE CONFIGURACOES FINAIS #################################
+
+callRequirements(){
+    setupEnvironment
+    setupOSRequirements
+    #setupGolang
+}
+
+callInstallTools(){
+    Altdns
+    Alterx
+    Amass
+    Anew
+    Antiburl
+    Arjun
+    Assetfinder
+    Burl
+    ChaosClient
+    Collector
+    DalFox
+    Dirsearch
+    Dnsx
+    ffuf
+    Findomains
+    Gau
+    Gauplus
+    Gf
+    GithubSearch
+    GitDorker
+    GitDumper
+    GoogleChrome
+    GoSpider
+    Gowitness
+    Hakrawler
+    Hakrevdns
+    Haktrails
+    Httprobe
+    Httpx
+    JSScanner
+    Kiterunner
+    LinkFinder
+    Mapcidr
+    Massdns
+    MegaPy
+    Metabigor
+    Metasploit
+    Naabu
+    Notify
+    Nrich
+    Nuclei
+    ParamSpider
+    ShufleDNS
+    Sub404
+    Subfinder
+    Subjs
+    Telegram-Send
+    TurboSearch
+    Qsreplace
+    Unfurl
+    Uro
+    Waybackurls
+    wafw00f
+    WPScan
+}
+
+
+callPosInstalacao(){
+    PosInstalacao
+
+}
+
+#Root or Sudoer verifying.
+if [[ $(id -u) != 0 ]]; then
+    echo -e "\n[!] Script precisa ser executado como sudoer ou root!"
+    exit 0
+else
+    callRequirements
+    #callInstallTools
+    #callPosInstalacao
+fi
